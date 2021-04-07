4f283c2bdc3b0827661d93d1705ab62658d1e190
董家祎 <>
2021-04-07 16:33:47 +0800

    no message

---
+++ ABCMediaPlay/ABCMediaPlay/source/ABCRecoderWAV.cpp
@@ -0,0 +1,26 @@
+//
+//  ABCRecoderWAV.cpp
+//  ABCMediaPlay
+//
+//  Created by 董家祎 on 2021/4/7.
+//
+
+#include "ABCRecoderWAV.hpp"
+#include <iostream>
+
+///一边录制pcm 一边转化为wav
+using namespace std;
+ABCRecoderWAV::ABCRecoderWAV(){
+    cout << "初始化ABCRecoderWAV" << endl;
+}
+
+ABCRecoderWAV::~ABCRecoderWAV(){
+    cout << "ABCRecoderWAV释放"<< endl;
+}
+void ABCRecoderWAV::startRecoder(){
+    
+}
+
+void ABCRecoderWAV::stopRecoder(){
+    
+}
---
+++ ABCMediaPlay/ABCMediaPlay/source/ABCRecoderWAV.hpp
@@ -0,0 +1,23 @@
+//
+//  ABCRecoderWAV.hpp
+//  ABCMediaPlay
+//
+//  Created by 董家祎 on 2021/4/7.
+//
+
+#ifndef ABCRecoderWAV_hpp
+#define ABCRecoderWAV_hpp
+
+#include <stdio.h>
+
+class ABCRecoderWAV{
+  
+public:
+    ABCRecoderWAV();
+    ~ABCRecoderWAV();
+    void startRecoder();
+    void stopRecoder();
+    
+};
+
+#endif /* ABCRecoderWAV_hpp */
